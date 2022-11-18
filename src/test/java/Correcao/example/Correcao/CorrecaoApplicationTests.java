3e2b24f8929ec0249e26bf9e9f638edb7768a254
ch-pereira <christofer.pereira.santos2403@gmail.com>
2022-11-18 19:05:00 -0300

    first commit

---
+++ src/test/java/Correcao/example/Correcao/CorrecaoApplicationTests.java
@@ -0,0 +1,13 @@
+package Correcao.example.Correcao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorrecaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
